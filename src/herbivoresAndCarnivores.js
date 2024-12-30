79be57679151b81f212a88377abfccee34cb28be
Veronica Shyian <veronica.shyian@gmail.com>
2024-12-30 20:55:24 +0200

    Bug fix

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -24,14 +24,10 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(animal) {
-    if (animal.hidden === false) {
+    if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
 
-      if (animal.health === 0) {
-        const index = Animal.alive.indexOf(animal);
-
-        Animal.alive.splice(index, 1);
-      }
+      Animal.alive = Animal.alive.filter((beast) => beast.health > 0);
     }
   }
 }
